8eaf25a30f205d1ee7d44932e0d1cc73616e99d7
bIropka <biropka@gmail.com>
2016-02-08 15:39:37 +0200

    v 0.3

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,3 +1,15 @@
-/**
- * Created by USER on 04.02.2016.
- */
+$(document).ready(function () {
+
+    $('.call-us-button').click(function () {
+        $('.window-callback').fadeIn();
+    });
+    $('.window-callback').click(function (event) {
+        $target = $(event.target);
+        if (!$target.closest($('.window-callback .call-us-form')).length) $('.window-callback').fadeOut();
+    });
+
+    jQuery(function($){
+        $('.user-phone').mask("+38(999) 999-99-99");
+    });
+
+});
